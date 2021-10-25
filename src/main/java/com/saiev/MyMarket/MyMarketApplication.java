962bb7902027e6be82e2048855d4ee4a0bcdc10b
EXPERT <saievmuhammad@mail.ru>
2021-10-25 16:17:08 +0300

    Done lesson homework 3

--- src/main/java/com/saiev/MyMarket/MyMarketApplication.java
+++ src/main/java/com/saiev/MyMarket/MyMarketApplication.java
@@ -1,13 +1,17 @@
 package com.saiev.MyMarket;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@ComponentScan("com.saiev.MyMarket")
+@EnableAutoConfiguration
+@EnableAspectJAutoProxy
 public class MyMarketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyMarketApplication.class, args);
 	}
-
 }
